3433e3209710eb7c8a6cd5e236543588a21fde77
Your Name <you@example.com>
2017-03-08 14:41:55 +0530

    Sine of an Angle using Taylors Series

---
+++ week2/sinetaylor.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+float convert_rad(float d)
+{
+float rad;
+rad=(3.14159/180)*d;
+return rad;
+}
+float sine(float x)
+{
+float term,sum,prev,diff;
+int i;
+term=x;
+sum=x;
+diff=x;
+for(i=3;diff>0.000001;i+=2)
+{
+prev=term;
+term=(-term*x*x)/(i*(i-1));
+diff=fabs(prev-term);
+sum=sum+term;
+}
+return sum;
+}
+int main()
+{
+float degree,x,sum,s;
+printf("Enter the Angle in Degree\n");
+scanf("%f",&degree);
+x=convert_rad(degree);
+sum=sine(x);
+printf("The sine of %.1f degrees using Taylors Series is %f \n",degree,sum);
+s=sin(x);
+printf("The sine of %.1f degrees using the function is %f",degree,s);
+return 0;
+}
+
