5d9f362b6002cfe47645ba4dfe1235451d8850d9
smn103 <samo16cs@cmrit.ac.in>
2017-04-04 14:59:32 +0530

    Bubble Sort

--- week4/bubblesort.c
+++ week4/bubblesort.c
@@ -20,9 +20,9 @@ void displayarray(int n, int a[n])
 void bubblesort(int n, int a[n])
 {
 int i,j,temp;
-for (i=0;i<(n-1);i++)
+for (i=1;i<n;i++)
 {
-    for (j=0;j<n-i-1;j++)
+    for (j=0;j<n-i;j++)
     {
       if (a[j]>a[j+1]) 
       {
@@ -49,5 +49,7 @@ int main()
 	bubblesort(n,a);
 	return 0;
 }
+	
+
 	
 
